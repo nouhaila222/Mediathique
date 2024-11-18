c987b75dcf63f708d68a0b4be29c622286731787
nouha ila <elmaksoubn@gmail.com>
2024-11-18 17:41:19 +0100

    add new lines

--- navbar.js
+++ navbar.js
@@ -4,14 +4,19 @@ export default function Navbar() {
   return (
     <nav>
        <ul className='navbar'>
-      <div className='loogo'>
-      <li>EpicReads</li>
-      </div>
-      <div className='nav'>
-      <li className='home'>home</li>
-      <li className='top'>top</li>
-      <li className='nouveau'>nouveaute</li>
-      </div>
+        <div className='loogo'>
+          <li>EpicReads</li>
+        </div>
+        <div className='nav'>
+          <li className='home'>home</li>
+          <li className='books'>books</li>
+          <li className='about'>about</li>
+          <li className='contact'>contact</li>
+        </div>
+        <div className='login'>
+          <a href='https://www.figma.com/design/GXz5UI4CMHRJbFjJehNwAT/Untitled?node-id=246-253&node-type=frame&t=oVkMze8lwILyThlH-0'>sign in</a>&nbsp;&nbsp;
+          <a href='https://www.figma.com/design/GXz5UI4CMHRJbFjJehNwAT/Untitled?node-id=246-253&node-type=frame&t=oVkMze8lwILyThlH-0'>sign up</a>
+        </div>
     </ul>
     </nav>
   )
