ff509e6db21016c5c7fd84d41dc556e985d643dc
nouha ila <elmaksoubn@gmail.com>
2024-11-24 02:30:54 +0100

    add new lines

--- section.js
+++ section.js
@@ -1,16 +1,28 @@
 import React from 'react'
-import remove  from "./remove.png"
+import design  from "./design.jpg"
 import './section.css'
 export default function Section () {
   return (
     <div className='section'>
     <div className='image'>
-        <img src={remove} alt='fille'></img>
+        <img src={design} alt='fille'></img>
     </div>
-    <div className='search'>
+
+    <div className='welcom'>
       <h1>Welcom To EpicReads</h1>
       <p>We're so glad you'e here</p>
       <input value={'Find Your Book'}></input>
+    </div>
+
+    <div className='description'>
+      <h1>
+        Find your next great<br/>
+        read at our online<br/>
+        books store
+      </h1>
+
+      <h2>Best Authors Book</h2>
+
     </div>
     </div>
   )
