f4866342bd080abf4adf3efadd2a29f1bf753308
nouha ila <elmaksoubn@gmail.com>
2024-11-25 18:11:36 +0100

    new lines

--- section.js
+++ section.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import design  from "./design.jpg"
+import design  from "./images/design.jpg"
+import book1  from "./images/book1.png"
+import book2  from "./images/book2.png"
+import book3  from "./images/book3.png"
+import book4  from "./images/book4.png"
 import './section.css'
 export default function Section () {
   return (
@@ -22,7 +26,31 @@ export default function Section () {
       </h1>
 
       <h2>Best Authors Book</h2>
+      <div className='popularbook'>
+        <div>
+        <img src={book1} alt='book'></img>
+        <p>Letting Go<br/>
+        by Philip Roth</p>
+        </div>
 
+        <div>
+        <img src={book2} alt='book'></img>
+        <p>Fear of Flying<br/>
+        by Erica Jong</p>
+        </div>
+        
+        <div>
+        <img src={book3} alt='book'></img>
+      <p>Encyclopedia Brown
+      <br/>by Donald J. Sobol</p>
+      </div>
+        
+        <div>
+          <img src={book4} alt='book'></img>
+          <p>When Breath Becomes Air<br/>
+          by Paul Kalanithi</p>
+        </div>
+    </div>
     </div>
     </div>
   )
