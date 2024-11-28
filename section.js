6a0c53212a28f680183fa4f387e6a1ed0a01d553
nouha ila <elmaksoubn@gmail.com>
2024-11-28 09:44:24 +0100

    new lines

--- section.js
+++ section.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import design  from "./images/design.jpg"
+import girl  from "./images/girl.png"
 import book1  from "./images/book1.png"
 import book2  from "./images/book2.png"
 import book3  from "./images/book3.png"
 import book4  from "./images/book4.png"
+import book5 from './images/book5.png'
+import photo1 from './images/photo1.jpg'
 import './section.css'
 export default function Section () {
   return (
     <div className='section'>
     <div className='image'>
-        <img src={design} alt='fille'></img>
+        <img src={girl} alt='fille'></img>
     </div>
 
     <div className='welcom'>
@@ -25,7 +27,8 @@ export default function Section () {
         books store
       </h1>
 
-      <h2>Best Authors Book</h2>
+      <h2 >Best Authors Book</h2>
+
       <div className='popularbook'>
         <div>
         <img src={book1} alt='book'></img>
@@ -40,18 +43,70 @@ export default function Section () {
         </div>
         
         <div>
-        <img src={book3} alt='book'></img>
-      <p>Encyclopedia Brown
-      <br/>by Donald J. Sobol</p>
+            <img src={book3} alt='book'></img>
+          <p>Encyclopedia Brown
+          <br/>by Donald J. Sobol</p>
       </div>
         
-        <div>
-          <img src={book4} alt='book'></img>
-          <p>When Breath Becomes Air<br/>
-          by Paul Kalanithi</p>
+          <div>
+            <img src={book4} alt='book'></img>
+            <p>When Breath Becomes Air<br/>
+            by Paul Kalanithi</p>
+          </div>
+
+          <div>
+          <img src={book5} alt='book'></img>
+          <p>In the Heart of the Sea<br/>
+          by Nathaniel Philbrick</p>
         </div>
+        </div>
+        <button>See All</button>
+
+        <h2>Best Selling Book</h2>
+
+        <div className='sellingbook'>
+          <div>
+          <img src={book4} alt='book'></img>
+            <p>When Breath Becomes Air<br/>
+            by Paul Kalanithi</p>
+          </div>
+
+        <div>
+          <img src={book1} alt='book'></img>
+          <p>Letting Go<br/>
+          by Philip Roth</p>
+        </div>
+
+        <div>
+          <img src={book2} alt='book'></img>
+          <p>Fear of Flying<br/>
+          by Erica Jong</p>
+        </div>
+        
+        <div>
+          <img src={book3} alt='book'></img>
+        <p>Encyclopedia Brown
+        <br/>by Donald J. Sobol</p>
+      </div>
+
+        <div>
+          <img src={book5} alt='book'></img>
+          <p>In the Heart of the Sea<br/>
+          by Nathaniel Philbrick</p>
+        </div>
+        </div>
+        <button>See All</button>
+        
     </div>
+    
+    <div className='desc'>
+      <img src={photo1} alt='bibliotheque'></img>
+      <h2>Discover a world of stories and imagination</h2>
+      <p>Best books, at the best prices - Enjoy discounts and special offers on thousands of titles.</p>
     </div>
-    </div>
+  </div>
+
+
   )
 }
+
